c68725f42f5e62e99bc7b87ae91db3b4431fc445
Philip Molares <philip.molares@udo.edu>
2019-09-15 18:40:50 +0200

    bump version to 0.3.1

--- vault_printer/__init__.py
+++ vault_printer/__init__.py
@@ -5,4 +5,4 @@ vault_printer module
 description = "A program to get all secrets from a vault servers kv_store for printing"  # pylint: disable=invalid-name
 name = "vault_printer"  # pylint: disable=invalid-name
 url = "https://github.com/DerMolly/vault_printer"  # pylint: disable=invalid-name
-version = "0.3.0"  # pylint: disable=invalid-name
+version = "0.3.1"  # pylint: disable=invalid-name
